190850b30b8a6116a974670e66e8914ea7c41ed5
hashiuchi <1510320368f@kindai.ac.jp>
2018-07-17 16:37:46 +0800

    パスワード長いか短いとできない機能の追加

--- resister/signup.php
+++ resister/signup.php
@@ -10,11 +10,18 @@
         if ($name == '') {
             $errors['name'] = 'blank';
         }
+
         if ($email == '') {
             $errors['email'] = 'blank';
         }
+
+        $count = strlen($password);
+
         if ($password == '') {
-            $password['password'] = 'blank';
+            $errors['password'] = 'blank';
+        }
+        elseif ($count < 4 || 16 < $count) {
+            $errors['password'] = 'length';
         }
     }
 ?>
@@ -52,6 +59,9 @@
                         <input type="password" name="input_password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
                         <?php if (isset($errors['password']) && $errors['password'] == 'blank'): ?>
                             <p class="text-danger">passwordを入力してください</p>
+                        <?php endif; ?>
+                        <?php if (isset($errors['password']) && $errors['password'] == 'length'): ?>
+                            <p class="text-danger">passwordが長すぎるか短すぎます</p>
                         <?php endif; ?>
                     </div>
                     <div class="form-group">
